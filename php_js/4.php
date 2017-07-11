8b825ec94f19218d3a965682685ae8e4f1cb3c18
harris <harrisleesh@gmail.com>
2017-07-11 14:51:08 -0700

    web_study

---
+++ php_js/4.php
@@ -0,0 +1,22 @@
+<?php
+  $name = "harris";
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script>
+        name = "harris";
+        document.write("안녕하세요."+name);
+    </script>
+    <h1>php</h1>
+    <?php
+      echo "안녕하세요.".$name;
+     ?>
+
+  </body>
+</html>
