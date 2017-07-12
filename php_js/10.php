db8a6ee91fac2e069f721d7f63b879d6089f1d89
harris <harrisleesh@gmail.com>
2017-07-12 16:27:15 -0700

    Everyday code

---
+++ php_js/10.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script>
+      list = new Array("one", "two", "three");
+      document.write(list.length);
+    </script>
+    <h1>php</h1>
+    <?php
+      $list = array("one","two","three");
+      echo count($list);
+    ?>
+  </body>
+</html>
